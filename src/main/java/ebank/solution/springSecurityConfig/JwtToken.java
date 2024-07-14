837e0bdc438b1a5ce67686fecae35872d1c99bfb
I M A N E <imanebahy91@gmail.com>
2024-07-14 21:05:33 +0100

    create a token function

--- src/main/java/ebank/solution/springSecurityConfig/JwtToken.java
+++ src/main/java/ebank/solution/springSecurityConfig/JwtToken.java
@@ -1,47 +1,47 @@
-//package ebank.solution.springSecurityConfig;
-//
-//
-//import com.nimbusds.jose.jwk.source.ImmutableSecret;
-//import ebank.solution.Dto.LoginDto;
-//import ebank.solution.Dto.RegisterDto;
-//import ebank.solution.model.User;
-//import ebank.solution.repository.UserRepository;
-//import jakarta.validation.Valid;
-//import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
-//import org.springframework.security.oauth2.jwt.JwsHeader;
-//import org.springframework.security.oauth2.jwt.JwtClaimsSet;
-//import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
-//import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
-//import org.springframework.validation.BindingResult;
-//import org.springframework.validation.FieldError;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.time.Instant;
-//import java.util.HashMap;
-//
-//@RestController
-//@RequestMapping("/jwt")
-//public class JwtToken {
-//
-//    @Value("${security.jwt.secret-key}")
-//    private String jwtSecretKey;
-//
-//    @Value("${security.jwt.issuer}")
-//    private String jwtIssuer;
-//
+package ebank.solution.springSecurityConfig;
+
+
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
+import ebank.solution.Dto.LoginDto;
+import ebank.solution.Dto.RegisterDto;
+import ebank.solution.model.User;
+import ebank.solution.repository.UserRepository;
+import jakarta.validation.Valid;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwsHeader;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.Instant;
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("/jwt")
+public class JwtToken {
+
+    @Value("${security.jwt.secret-key}")
+    private String jwtSecretKey;
+
+    @Value("${security.jwt.issuer}")
+    private String jwtIssuer;
+
 //    @Autowired
 //    private UserRepository userRepository;
 //
 //    @Autowired
 //    private AuthenticationManager authenticationManager;
-//
+
 //
 ////    @GetMapping("/profile")
 ////    public ResponseEntity<Object> profile(Authentication auth) {
@@ -137,28 +137,28 @@
 //        return ResponseEntity.badRequest().body("Bad username or password");
 //
 //    }
-//
-//
-//
-//
-//
-//
-//    private  String createJwtToken(User user) {
-//        Instant now = Instant.now();
-//
-//        JwtClaimsSet claims = JwtClaimsSet.builder()
-//                .issuer(jwtIssuer)
-//                .issuedAt(now)
-//                .expiresAt(now.plusSeconds(24*3600))
-//                .subject(user.getUserName())
-//                .build();
-//
-//        var encoder= new NimbusJwtEncoder(
-//                new ImmutableSecret<>(jwtSecretKey.getBytes()));
-//
-//        var params = JwtEncoderParameters.from(
-//                JwsHeader.with(MacAlgorithm.HS256).build(),claims);
-//
-//        return encoder.encode(params).getTokenValue();
-//    }
-//}
+
+
+
+
+
+
+    private  String createJwtToken(User user) {
+        Instant now = Instant.now();
+
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer(jwtIssuer)
+                .issuedAt(now)
+                .expiresAt(now.plusSeconds(24*3600))
+                .subject(user.getUserName())
+                .build();
+
+        var encoder= new NimbusJwtEncoder(
+                new ImmutableSecret<>(jwtSecretKey.getBytes()));
+
+        var params = JwtEncoderParameters.from(
+                JwsHeader.with(MacAlgorithm.HS256).build(),claims);
+
+        return encoder.encode(params).getTokenValue();
+    }
+}
